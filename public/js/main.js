4d09aafb719b6044309c77e8a22805b6868ed592
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2013-10-13 20:12:18 -0300

    Map with custom tiles

--- public/js/main.js
+++ public/js/main.js
@@ -3,14 +3,40 @@ var mapMinZoom = 5;
 var mapMaxZoom = 15;
 
 var map;
+var maptiler = new google.maps.ImageMapType({
+    getTileUrl: function(coord, zoom) {
+        var proj = map.getProjection();
+        var z2 = Math.pow(2, zoom);
+        var tileXSize = 256 / z2;
+        var tileYSize = 256 / z2;
+        var tileBounds = new google.maps.LatLngBounds(
+            proj.fromPointToLatLng(new google.maps.Point(coord.x * tileXSize, (coord.y + 1) * tileYSize)),
+            proj.fromPointToLatLng(new google.maps.Point((coord.x + 1) * tileXSize, coord.y * tileYSize))
+        );
+        var ymax = 1 << zoom;
+        var y = ymax - coord.y -1;
+        if (mapBounds.intersects(tileBounds) && (mapMinZoom <= zoom) && (zoom <= mapMaxZoom))
+            return "img/tiles/sample/urban_footprint/t0/" + zoom + "/" + coord.x + "/" + y + ".png";
+        else
+            return "http://www.maptiler.org/img/none.png";
+    },
+    tileSize: new google.maps.Size(256, 256),
+    isPng: true,
+
+    opacity: 0.5
+})
+
 function initialize() {
+
     var mapOptions = {
         zoom: 12,
         center: mapBounds.getCenter(),
-        mapTypeId: google.maps.MapTypeId.SATELLITE
+        mapTypeId: google.maps.MapTypeId.SATELLITE,
     };
-    map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    map.setMapTypeId('satellite');
+    map.overlayMapTypes.insertAt(0, maptiler);
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
