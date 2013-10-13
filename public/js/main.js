27a50c220c14ddaebc90be2fcf94559359f2485c
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2013-10-13 19:12:00 -0300

    Sample base map

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,16 @@
+var mapBounds = new google.maps.LatLngBounds(new google.maps.LatLng(-33.3622363713, -66.4197717386), new google.maps.LatLng(-33.2251227271, -66.2673854768));
+var mapMinZoom = 5;
+var mapMaxZoom = 15;
 
+var map;
+function initialize() {
+    var mapOptions = {
+        zoom: 12,
+        center: mapBounds.getCenter(),
+        mapTypeId: google.maps.MapTypeId.SATELLITE
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
