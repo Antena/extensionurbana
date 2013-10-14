c80c8f22c53e40b16249b713dba48d9983460928
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2013-10-14 17:20:09 -0300

    Layer opacity control

--- public/js/google-map.js
+++ public/js/google-map.js
@@ -5,47 +5,24 @@ angular.module('google.map', []).value('mapOptions',{}).directive('googleMap', [
         compile: function(element, attributes) {
             return function(scope, elem, attrs, ngModel) {
 
-
-                var mapBounds = new google.maps.LatLngBounds(new google.maps.LatLng(-33.3622363713, -66.4197717386), new google.maps.LatLng(-33.2251227271, -66.2673854768));
-                var mapMinZoom = 5;
-                var mapMaxZoom = 15;
-
-                var map;
-                var maptiler = new google.maps.ImageMapType({
-                    getTileUrl: function(coord, zoom) {
-                        var proj = map.getProjection();
-                        var z2 = Math.pow(2, zoom);
-                        var tileXSize = 256 / z2;
-                        var tileYSize = 256 / z2;
-                        var tileBounds = new google.maps.LatLngBounds(
-                            proj.fromPointToLatLng(new google.maps.Point(coord.x * tileXSize, (coord.y + 1) * tileYSize)),
-                            proj.fromPointToLatLng(new google.maps.Point((coord.x + 1) * tileXSize, coord.y * tileYSize))
-                        );
-                        var ymax = 1 << zoom;
-                        var y = ymax - coord.y -1;
-                        if (mapBounds.intersects(tileBounds) && (mapMinZoom <= zoom) && (zoom <= mapMaxZoom))
-                            return "img/tiles/sample/urban_footprint/t0/" + zoom + "/" + coord.x + "/" + y + ".png";
-                        else
-                            return "http://www.maptiler.org/img/none.png";
-                    },
-                    tileSize: new google.maps.Size(256, 256),
-                    isPng: true,
-
-                    opacity: 0.5
-                })
+                scope.mapOptions = {
+                    mapBounds : new google.maps.LatLngBounds(new google.maps.LatLng(-33.3622363713, -66.4197717386), new google.maps.LatLng(-33.2251227271, -66.2673854768)),
+                    mapMinZoom : 5,
+                    mapMaxZoom : 15
+                }
 
                 function initialize() {
-
                     var mapOptions = {
-                        zoom: 12,
-                        center: mapBounds.getCenter(),
+                        zoom: parseInt(attrs.zoom),
+                        center: scope.mapOptions.mapBounds.getCenter(),
                         mapTypeId: google.maps.MapTypeId.SATELLITE,
-                        minZoom: mapMinZoom,
-                        maxZoom: mapMaxZoom,
+                        minZoom: scope.mapOptions.mapMinZoom,
+                        maxZoom: scope.mapOptions.mapMaxZoom,
                         streetViewControl: false
                     };
-                    map = new google.maps.Map($(elem)[0], mapOptions);
-                    map.overlayMapTypes.insertAt(0, maptiler);
+
+                    scope.map = new google.maps.Map($(elem)[0], mapOptions);
+                    scope.initLayers();
                 }
 
                 // Late-bind to prevent compiler clobbering
--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,55 @@
 // Atlas app
-var atlasApp = angular.module('atlas', ['atlas.controllers', 'atlas.directives', 'ui.slider', 'google.map']);
-
-// Controllers
-var controllers = angular.module('atlas.controllers', []);
-
-controllers.controller('AppController', ['$scope', function($scope) {
-//    console.log($scope.map);        //TODO(gb): Remove trace!!!
-}])
-
-controllers.controller('MapController', ['$scope', function($scope) {
-
-}])
-
-controllers.controller('LayerController', ['$scope', function($scope) {
-    $scope.urbanFootprintOpacity = 0.5;
-
-}])
+var atlasApp = angular.module('atlas', ['atlas.controllers', 'atlas.directives', 'atlas.factories', 'ui.slider', 'google.map']);
 
 // Directives
 var directives = angular.module('atlas.directives', []);
+
+// Factories
+var factories = angular.module('atlas.factories', []);
+
+factories.factory('TileLayer', [function() {
+    return {
+        create: function(map, options, opacity) {
+            return new google.maps.ImageMapType({
+                getTileUrl: function(coord, zoom) {
+                    var proj = map.getProjection();
+                    var z2 = Math.pow(2, zoom);
+                    var tileXSize = 256 / z2;
+                    var tileYSize = 256 / z2;
+                    var tileBounds = new google.maps.LatLngBounds(
+                        proj.fromPointToLatLng(new google.maps.Point(coord.x * tileXSize, (coord.y + 1) * tileYSize)),
+                        proj.fromPointToLatLng(new google.maps.Point((coord.x + 1) * tileXSize, coord.y * tileYSize))
+                    );
+                    var ymax = 1 << zoom;
+                    var y = ymax - coord.y -1;
+                    if (options.mapBounds.intersects(tileBounds) && (options.mapMinZoom <= zoom) && (zoom <= options.mapMaxZoom))
+                        return "img/tiles/sample/urban_footprint/t0/" + zoom + "/" + coord.x + "/" + y + ".png";
+                    else
+                        return "http://www.maptiler.org/img/none.png";
+                },
+                tileSize: new google.maps.Size(256, 256),
+                isPng: true,
+
+                opacity: opacity
+            });
+        }
+    }
+}])
+
+// Controllers
+var controllers = angular.module('atlas.controllers', []);
+controllers.controller('AppController', ['$scope',  'TileLayer', function($scope,  TileLayer) {
+    $scope.layerOpacity = 0.5;
+
+    $scope.$watch('layerOpacity', function(oldValue, newValue) {
+        if ($scope.map) {
+            $scope.map.overlayMapTypes.getAt(0).setOpacity(newValue)
+        }
+    })
+
+    $scope.initLayers = function() {
+        var layer = TileLayer.create($scope.map, $scope.mapOptions, $scope.layerOpacity);
+        $scope.map.overlayMapTypes.insertAt(0, layer);
+        $scope.layer = layer;
+    }
+}])
