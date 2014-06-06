66797d20ec487da95878380ec5683e289797b821
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2014-06-06 17:27:59 -0300

    New development chart

--- public/js/vis.js
+++ public/js/vis.js
@@ -2,7 +2,7 @@ var vis = angular.module('atlas.vis', []);
 
 vis.directive('fragmentationChart', function() {
 
-    var margin = {top: 20, right: 25, bottom: 20, left: 25},
+    var margin = {top: 20, right: 42, bottom: 20, left: 78},
         width = 250 - margin.left - margin.right,
         height = 140 - margin.top - margin.bottom;
 
@@ -127,7 +127,7 @@ vis.directive('builtupChart', function() {
         height = 140 - margin.top - margin.bottom;
 
     var x = d3.scale.ordinal()
-        .rangeRoundBands([0, width], .1);
+        .rangeRoundBands([0, width], .3);
 
     var y = d3.scale.linear()
         .rangeRound([height, 0]);
@@ -166,6 +166,12 @@ vis.directive('builtupChart', function() {
             svg.append("g")
                 .attr("class", "y axis")
                 .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .text("Has");
 
             scope.$watch('data', function(city) {
                 if (!city) return;
@@ -197,7 +203,6 @@ vis.directive('builtupChart', function() {
                     d.values = color.domain().map(function(type) { return { type: type, y0: y0, y1: y0 += +d[type] }});
                     d.total = d.values[d.values.length - 1].y1;
                 })
-                data.sort(function(a, b) { return b.total - a.total; });
 
                 x.domain(data.map(function(d) { return d.year; }));
                 y.domain([0, d3.max(data, function(d) { return d.total; })]);
@@ -205,6 +210,125 @@ vis.directive('builtupChart', function() {
                 svg.select(".y.axis")
                     .transition().duration(750)
                     .call(yAxis);
+
+                svg.select(".x.axis")
+                    .call(xAxis);
+
+                var builtUpGroups = svg.selectAll(".year-bargroup")
+                    .data(data)
+
+                builtUpGroups.enter().append("g")
+                    .attr("class", "year-bargroup")
+                    .attr("transform", function(d, i) {
+                        return "translate(" + x(d.year) + ",0)";
+                    })
+
+                var builtUpRects = builtUpGroups.selectAll("rect")
+                    .data(function(d) { return d.values; })
+
+                builtUpRects.enter().append("rect")
+                    .attr("width", x.rangeBand())
+                    .attr("y", height)
+                    .attr("height", 0)
+                    .style("fill", function(d) { return color(d.type); })
+
+                builtUpRects
+                    .transition()
+                    .delay(500)
+                    .attr("y", function(d) { return y(d.y1); })
+                    .attr("height", function(d) { return y(d.y0) - y(d.y1); })
+            })
+        }
+    }
+})
+
+vis.directive('newdevelopmentChart', function() {
+
+    var margin = {top: 20, right: 0, bottom: 20, left: 35},
+        width = 250 - margin.left - margin.right,
+        height = 140 - margin.top - margin.bottom;
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .3);
+
+    var y = d3.scale.linear()
+        .rangeRound([height, 0]);
+
+    var color = d3.scale.ordinal()
+        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .tickFormat(d3.format(".2s"));
+
+    return {
+        restrict: 'E',
+        scope: {
+            data: '='
+        },
+
+        link: function(scope, element, attrs) {
+
+            var svg = d3.select($(element)[0]).append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
+
+            svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .text("Has");
+
+            scope.$watch('data', function(city) {
+                if (!city) return;
+
+                var data = [
+                    {
+                        year: "1990-2000",
+                        infill: parseFloat(city.t0_t1_infill),
+                        extension: parseFloat(city.t0_t1_extension),
+                        leapfrog: parseFloat(city.t0_t1_leapfrog)
+                    },
+                    {
+                        year: "2000-2010",
+                        infill: parseFloat(city.t1_t2_infill),
+                        extension: parseFloat(city.t1_t2_extension),
+                        leapfrog: parseFloat(city.t1_t2_leapfrog)
+                    }
+                ];
+
+                color.domain(d3.keys(data[0]).filter(function(key) { return key !== "year"; }));
+                data.forEach(function(d) {
+                    var y0 = 0;
+                    d.values = color.domain().map(function(type) { return { type: type, y0: y0, y1: y0 += +d[type] }});
+                    d.total = d.values[d.values.length - 1].y1;
+                })
+
+                x.domain(data.map(function(d) { return d.year; }));
+                y.domain([0, d3.max(data, function(d) { return d.total; })]);
+
+                svg.select(".y.axis")
+                    .transition().duration(750)
+                    .call(yAxis);
+
+                svg.select(".x.axis")
+                    .call(xAxis);
 
                 var builtUpGroups = svg.selectAll(".year-bargroup")
                     .data(data)
